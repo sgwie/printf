201dade94a0db3e5ee9bdc6e7678479580476dc3
Shay-Lassie <lassie034@gmail.com>
2022-10-18 15:24:02 +0000

    fixed _putchar.c betty errors

--- _putchar.c
+++ _putchar.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
+/**
+ * _putchar - produces output to stdout
+ * Description: returns nuber of charactes
+ * @c: input strings to print
+ * Return: characters to print to stdout
+ */
+
 int _putchar(char c)
 {
-  return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
