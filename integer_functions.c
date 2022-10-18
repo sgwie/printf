f7e53b2a297dffeff870d8bfb573e3c40f62db1b
Shay-Lassie <lassie034@gmail.com>
2022-10-18 16:57:31 +0000

    betty for integer

--- integer_functions.c
+++ integer_functions.c
@@ -1,41 +1,40 @@
 #include "main.h"
 
 /**
- *print_number - Prints integer number
- *
- *@n: number to be printed
- *
- *Return: Integer number
+ * print_number - Prints integer number
+ * Description: function that prints an intreger
+ * @args: number to be printed
+ * Return: Integer number
  */
 
 int print_number(va_list args)
 {
-  int n;
-  int divisor;
-  int length;
-  unsigned int number;
+	int n;
+	int divisor;
+	int length;
+	unsigned int number;
 
-  n  = va_arg(args, int);
-  divisor = 1;
-  length = 0;
+	n  = va_arg(args, int);
+	divisor = 1;
+	length = 0;
 
-  if (n < 0)
-    {
-      length += _putchar('-');
-      number = n * -1;
-    }
-  else
-    number = n;
+	if (n < 0)
+	{
+		length += _putchar('-');
+		number = n * -1;
+	}
+	else
+		number = n;
 
-  for (; number / divisor > 9; )
-    divisor *= 10;
+	for (; number / divisor > 9; )
+		divisor *= 10;
 
-  for (; divisor != 0; )
-    {
-      length += _putchar('0' + number / divisor);
-      number %= divisor;
-      divisor /= 10;
-    }
+	for (; divisor != 0; )
+	{
+		length += _putchar('0' + number / divisor);
+		number %= divisor;
+		divisor /= 10;
+	}
 
-  return (length);
+	return (length);
 }
