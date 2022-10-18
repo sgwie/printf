6ccfeee3beef8202ec47302740d7d07ce0071c9a
Shay-Lassie <lassie034@gmail.com>
2022-10-18 15:33:46 +0000

    formatting _printf.c

--- _printf.c
+++ _printf.c
@@ -1,22 +1,31 @@
 #include "main.h"
+
+/**
+ * _printf - prints output according to a certain format
+ * Description: does not print the final null byte
+ * @format: format in which to print
+ * Return: Always 0
+ */
+
 int _printf(const char *format, ...)
 {
-int printchar;
-prints_t func_list[] = {
-		     {"c", print_char},
-		     {"s", print_string},
-		     {"%", print_percent},
-		     {"d", print_integer},
-		     {NULL, NULL},
-};
-va_list arg_list;
+	int printchar;
+	prints_t func_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{NULL, NULL},
+	};
+	va_list arg_list;
 
-if (format == NULL)
-  return (-1);
+	if (format == NULL)
+	return (-1);
 
-va_start (arg_list, format);
+	va_start(arg_list, format);
 
-printchar = checker (format,func_list, arg_list);
-va_end(arg_list);
-return(printchar);
+	printchar = checker(format, func_list, arg_list);
+	va_end(arg_list);
+
+	return (printchar);
 }
