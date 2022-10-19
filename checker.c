7c63ee8a9e9917fc0537ed749b8454dd23a4ef8e
Shay-Lassie <lassie034@gmail.com>
2022-10-19 07:44:20 +0000

    last night's changes

--- checker.c
+++ checker.c
@@ -1,44 +1,51 @@
 #include "main.h"
 
+/**
+ * checker - checks content for formatting
+ * Description: updates symbols
+ * @format: specific format
+ * Return: answer
+ */
+
 int checker(const char *format, prints_t func_list[], va_list arg_list)
 {
-  int i, j, find, printchar;
+	int i, j, find, printchar;
 
-  printchar = 0;
-  for (i = 0; format[i] != '\0'; i++)/* Iterates through the main str*/
-    {
-      if (format[i] == '%') /*Checks for format specifiers*/
+	printchar = 0;
+	for (i = 0; format[i] != '\0'; i++)/* Iterates through the main str*/
 	{
-	  /*Iterates through struct to find the right func*/
-	  for (j = 0; func_list[j].symbol != NULL; j++)
-	    {
-	      if (format[i + 1] == func_list[j].symbol[0])
+		if (format[i] == '%') /*Checks for format specifiers*/
 		{
-		  find = func_list[j].func(arg_list);
-		  if (find == -1)
-		    return (-1);
-		  printchar += find;
-		  break;
+			for (j = 0; func_list[j].symbol != NULL; j++)
+			{
+				if (format[i + 1] == func_list[j].symbol[0])
+				{
+					find = func_list[j].func(arg_list);
+					if (find == -1)
+						return (-1);
+					printchar += find;
+					break;
+				}
+			}
+			if (func_list[j].symbol == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					printchar = printchar + 2;
+				}
+				else
+					return (-1);
+			}
+			i = i + 1; /*Updating i to skip format symbolbols*/
 		}
-	    }
-	  if (func_list[j].symbol == NULL && format[i + 1] != ' ')
-	    {
-	      if (format[i + 1] != '\0')
+		else
 		{
-		  _putchar(format[i]);
-		  _putchar(format[i + 1]);
-		  printchar = printchar + 2;
+		_putchar(format[i]); /*call the putchar function*/
+		printchar++;
 		}
-	      else
-		return (-1);
-	    }
-	  i = i + 1; /*Updating i to skip format symbolbols*/
 	}
-      else
-	{
-	  _putchar(format[i]); /*call the putchar function*/
-	  printchar++;
-	}
-    }
-  return (printchar);
+
+	return (printchar);
 }
